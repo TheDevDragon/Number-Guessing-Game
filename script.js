a095b243ab04ae3c3d9ba50d02a4abdfc8cb8c0e
TheDevDragon <98497922+TheDevDragon@users.noreply.github.com>
2022-01-26 18:56:11 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,14 @@ let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
+// Generates a random target number
 const generateTarget = () => {
     return Math.floor(Math.random() * 10)
 }
 
+/* This function compares the number choose from the human and computer
+   as well as which guess was closer to the secret target number
+*/
 const compareGuesses = (userGuess, computerGuess, secret) => {
     let user1 = Math.abs(userGuess - secret)
     let comp1 = Math.abs(comp1 - secret)
@@ -13,9 +17,22 @@ const compareGuesses = (userGuess, computerGuess, secret) => {
         return 'Please pick a number between 0 and 9!'
     }
 
-    if ()
+    if (user1 < comp1 || user1 === comp1) {
+        return true;
+    } else if (comp1 < user1) {
+        return false;
+    }
 }
 
+// This updateScore function updates the score based on whether the human or computer wins
 const updateScore = winner => {
+    if (winner === 'human') {
+        humanScore += 1;
+    } else {
+        computerScore +1;
+}
 
+// Advances the round Number    
+const advanceRound = () => {
+    currentRoundNumber += 1;
 }
